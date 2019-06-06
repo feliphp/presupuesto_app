3acc6a72716d1a8ee27fa1e0e64c4df3c3f439a9
feliphp <felsul@hotmail.com>
2019-06-06 09:51:30 -0500

    first commit

---
+++ app/egreso/egreso.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Egreso } from './egreso.model';
+import { EgresoServicio } from './egreso.servicio';
+
+@Component({
+  selector: 'app-egreso',
+  templateUrl: './egreso.component.html',
+  styleUrls: ['./egreso.component.css']
+})
+export class EgresoComponent implements OnInit {
+egresos:Egreso[] = [];
+@Input() ingresoTotal:number;
+
+  constructor(private egresoService:EgresoServicio) { }
+
+  ngOnInit() {
+    this.egresos = this.egresoService.egresos;
+  }
+
+  eliminarRegistro(egreso:Egreso){
+    this.egresoService.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso: Egreso){
+    return egreso.valor / this.ingresoTotal;
+  }
+
+}
